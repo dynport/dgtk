2e377f859b419689caa1b54034bef992b6c41879
Tobias Schwab <tobias.schwab@dynport.de>
2013-11-14 10:20:27 +0100

    add extra (optional) statistic attributes

--- es/entry.go
+++ es/entry.go
@@ -3,8 +3,13 @@ package es
 type Entries []*Entry
 
 type Entry struct {
-	TimestampMicros int64 `json:"time"`
-	Count           int   `json:"count"`
+	TimestampMicros int64   `json:"time"`
+	Count           int64   `json:"count"`
+	Min             float64 `json:"min,omitempty"`
+	Max             float64 `json:"max,omitempty"`
+	Total           float64 `json:"total,omitempty"`
+	TotalCount      int64   `json:"total_count,omitempty"`
+	Mean            float64 `json:"mean,omitempty"`
 }
 
 func (entry *Entry) Timestamp() int64 {
