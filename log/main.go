cdc9f44b4c3ae45d79d80cacead30d22a1c3357e
Tobias Schwab <tobias.schwab@dynport.de>
2013-12-04 12:43:30 +0100

    yet another logger

---
+++ log/main.go
@@ -0,0 +1,17 @@
+package log
+
+import (
+	"fmt"
+)
+
+func Error(format string, i ...interface{}) {
+	Log("ERROR", format, i...)
+}
+
+func Info(format string, i ...interface{}) {
+	Log("INFO ", format, i...)
+}
+
+func Log(tag, format string, i ...interface{}) {
+	fmt.Printf(tag+": "+format+"\n", i...)
+}
