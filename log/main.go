7119eb436fce1e64f8e5e760ae4646fe0f6275c7
Tobias Schwab <tobias.schwab@dynport.de>
2013-12-06 10:10:45 +0100

    add Fatal method for logging

--- log/main.go
+++ log/main.go
@@ -2,10 +2,16 @@ package log
 
 import (
 	"fmt"
+	"os"
 )
 
 func Error(format string, i ...interface{}) {
 	Log("ERROR", format, i...)
+}
+
+func Fatal(format string, i ...interface{}) {
+	Error(format, i...)
+	os.Exit(1)
 }
 
 func Info(format string, i ...interface{}) {
