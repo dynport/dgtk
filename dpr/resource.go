586878d16d8e0e9a6c7ed4ec4b8b1134dc23bad8
Tobias Schwab <tobias.schwab@dynport.de>
2014-01-23 08:19:49 +0100

    move resource into separate file

---
+++ dpr/resource.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+)
+
+type Resource struct {
+	*http.Request
+}
+
+func (r *Resource) localPath() string {
+	normalizedPath := root + r.Request.URL.Path
+	if strings.HasSuffix(normalizedPath, "/") {
+		normalizedPath += "index"
+	}
+	return normalizedPath
+}
+
+func (r *Resource) localHeadersPath() string {
+	return r.localPath() + ".headers"
+}
+
+func (r *Resource) store() error {
+	if e := r.storeHeaders(); e != nil {
+		return e
+	}
+	if e := r.storeBody(); e != nil {
+		return e
+	}
+	return nil
+}
+
+func (r *Resource) storeBody() error {
+	f, e := r.createFile(r.localPath())
+	if e != nil {
+		return e
+	}
+	defer f.Close()
+	i, e := io.Copy(f, r.Body)
+	if e != nil {
+		return e
+	}
+	log.Printf("stored %d byte", i)
+	return nil
+}
+
+func (r *Resource) createFile(p string) (io.WriteCloser, error) {
+	if e := os.MkdirAll(path.Dir(p), 0755); e != nil {
+		return nil, e
+	}
+	log.Printf("creating file %s", p)
+	return os.Create(p)
+}
+
+func (r *Resource) storeHeaders() error {
+	f, e := r.createFile(r.localHeadersPath())
+	if e != nil {
+		return e
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(r.Header)
+}
+
+func (r *Resource) Write(w http.ResponseWriter) (int64, error) {
+	log.Println("sending " + r.localPath())
+	f, e := os.Open(r.localPath())
+	if e != nil {
+		return 0, e
+	}
+	defer f.Close()
+	stat, e := f.Stat()
+	if e != nil {
+		return 0, e
+	}
+	w.Header().Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
+	if strings.HasSuffix(r.Request.URL.String(), "/json") {
+		stat, e := os.Stat(path.Dir(r.localPath()) + "/layer")
+		if e == nil {
+			w.Header().Add("X-Docker-Size", strconv.FormatInt(stat.Size(), 10))
+		}
+		w.Header().Set("Content-Type", "application/json")
+	}
+	w.WriteHeader(200)
+	i, e := io.Copy(w, f)
+	log.Printf("sent %d byte", i)
+	return i, e
+}
+
+func (r *Resource) Exists() bool {
+	return fileExists(r.localPath())
+}
