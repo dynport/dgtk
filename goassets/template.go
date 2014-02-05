f2b843449c955b5345a111340fcfb549d047c97c
Gereon Frey <gereon.frey@dynport.de>
2014-02-05 10:12:18 +0100

    major change to goassets
    Using the idea of a custom filesystem for testing (see the `10 things you (probably) don't know about go` talk) the
    assets code was rewritten:
    
    * If the GOASSETS_<pkg_name>_PATH is set that is used for retrieving the files.
    * Otherwise there is a filesystem like interface for the assets. Use something like `Open("asset_name")` and the
      `Read` method of the returned object.

--- goassets/template.go
+++ goassets/template.go
@@ -7,45 +7,117 @@ import (
 	"compress/gzip"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-var assets = map[string][]byte{}
+type assetFileSystemI interface {
+	Open(name string) (assetFileI, error)
+	AssetNames() []string
+}
+
+type assetFileI interface {
+	io.Closer
+	io.Reader
+}
+
+var assets assetFileSystemI
 
 func assetNames() (names []string) {
-	for name, _ := range assets {
-		names = append(names, name)
+	return assets.AssetNames()
+}
+
+func readAsset(key string) ([]byte, error) {
+	r, e := assets.Open(key)
+	if e != nil {
+		return nil, e
+	}
+	defer r.Close()
+
+	p, e := ioutil.ReadAll(r)
+	if e != nil {
+		return nil, e
+	}
+	return p, nil
+}
+
+func mustReadAsset(key string) []byte {
+	p, e := readAsset(key)
+	if e != nil {
+		panic(e)
+	}
+	return p
+}
+
+type assetOsFS struct{ root string }
+
+func (aFS assetOsFS) Open(name string) (assetFileI, error) {
+	return os.Open(filepath.Join(aFS.root, name))
+}
+
+func (aFS *assetOsFS) AssetNames() ([]string) {
+	names, e := filepath.Glob(aFS.root + "/*")
+	if e != nil {
+		log.Print(e)
 	}
 	return names
 }
 
-func mustReadAsset(key string) []byte {
-	content, e := readAsset(key)
-	if e != nil {
-		panic(e)
-	}
-	return content
+type assetIntFS map[string][]byte
+
+type assetFile struct {
+	decompressor io.ReadCloser
 }
 
-func readAsset(key string) ([]byte, error) {
-	b, ok := assets[key]
-	if !ok {
-		return nil, fmt.Errorf("asset %s not found in %v", key, assetNames())
+func (afs assetIntFS) AssetNames() (names []string) {
+	names = make([]string, 0, len(afs))
+	for k, _ := range afs {
+		names = append(names, k)
 	}
-	gz, err := gzip.NewReader(bytes.NewBuffer(b))
-	if err != nil {
-		return nil, fmt.Errorf("Decompression failed: %s", err.Error())
-	}
+	return names
+}
 
-	var buf bytes.Buffer
-	io.Copy(&buf, gz)
-	gz.Close()
-	return buf.Bytes(), nil
+func (afs assetIntFS) Open(name string) (af assetFileI, e error) {
+	if asset, found := afs[name]; found {
+		decomp, e := gzip.NewReader(bytes.NewBuffer(asset))
+		if e != nil {
+			return nil, e
+		}
+		af = &assetFile{decompressor: decomp}
+		return af, nil
+	}
+	return nil, os.ErrNotExist
+}
+
+func (a *assetFile) Close() error {
+	if a.decompressor != nil {
+		return a.decompressor.Close()
+	}
+	return nil
+}
+
+func (a *assetFile) Read(p []byte) (n int, e error) {
+	if a.decompressor == nil {
+		return 0, os.ErrInvalid
+	}
+	return a.decompressor.Read(p)
 }
 
 func init() {
-	{{ range .Assets }}assets["{{ .Key }}"] = []byte{
-{{ .Bytes }}
+	path := os.Getenv(fmt.Sprintf("GOASSETS_%s_PATH", strings.ToUpper("{{ .Package }}")))
+	if path != "" {
+		assets = &assetOsFS{root: path}
+		return
+	}
+
+	assetsTmp := assetIntFS{}
+	{{ range .Assets }}assetsTmp["{{ .Key }}"] = []byte{
+		{{ .Bytes }}
 	}
 	{{ end }}
+	assets = assetsTmp
 }
 `
