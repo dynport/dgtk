3103a369a1560ec15c6a7050a4041330fd4a11ab
Tobias Schwab <tobias.schwab@dynport.de>
2014-06-05 23:58:54 +0200

    add Progress tracker

---
+++ stats/progress.go
@@ -0,0 +1,37 @@
+package stats
+
+import "time"
+
+func NewProgress(total int) *progress {
+	return &progress{
+		total: total, started: time.Now(),
+	}
+}
+
+type progress struct {
+	total   int
+	count   int
+	started time.Time
+}
+
+func (p *progress) Inc() {
+	p.count++
+}
+
+func (s *progress) Count() int {
+	return s.count
+}
+
+func (s *progress) Total() int {
+	return s.total
+}
+
+func (s *progress) ToGo() time.Duration {
+	pending := float64(s.total - s.count)
+	return time.Duration(pending/s.PerSecond()) * time.Second
+}
+
+func (s *progress) PerSecond() float64 {
+	diff := time.Since(s.started)
+	return float64(s.count) / diff.Seconds()
+}
