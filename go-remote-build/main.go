80ae6f8c6534c7c333b977320f55955bfd1192f2
Tobias Schwab <tobias.schwab@dynport.de>
2014-10-06 15:22:24 +0200

    remove debug message

--- go-remote-build/main.go
+++ go-remote-build/main.go
@@ -51,7 +51,6 @@ type build struct {
 
 func benchmark(message string) func() {
 	started := time.Now()
-	logger.Printf("started  %s", message)
 	return func() {
 		logger.Printf("finished %s in %.06f", message, time.Since(started).Seconds())
 	}
