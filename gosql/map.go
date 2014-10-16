7127a4d0e0d6c838d47cae6bcc5abb97d4906846
Tobias Schwab <tobias.schwab@dynport.de>
2014-10-16 10:56:27 +0200

    update gosql.Map
    * rename Columns to ColumnValues
    * add CreateTableStatement
    * add InsertStatement

--- gosql/map.go
+++ gosql/map.go
@@ -1,10 +1,79 @@
 package gosql
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+)
+
+type Valuer interface {
+	Values() Map
+}
+type JsonType string
 
 type Map map[string]interface{}
 
-func (m Map) Columns(names ...string) (Columns, error) {
+type QueryOption struct {
+	IfNotExists bool
+}
+
+func IfNotExists(o *QueryOption) {
+	o.IfNotExists = true
+}
+
+func (m Map) CreateTableStatement(name string, opts ...func(*QueryOption)) (string, error) {
+	opt := &QueryOption{}
+	for _, o := range opts {
+		o(opt)
+	}
+	cols, e := m.Columns()
+	if e != nil {
+		return "", e
+	}
+	names := []string{}
+	for _, c := range cols {
+		dbType := ""
+		switch t := c.Value.(type) {
+		case JsonType:
+			dbType = "JSON"
+		case string:
+			dbType = "VARCHAR"
+		case int:
+			dbType = "INTEGER"
+		case time.Time:
+			dbType = "TIMESTAMP WITH TIME ZONE"
+		default:
+			_ = t
+			return "", fmt.Errorf("unable to handle type %T", c.Value)
+		}
+		names = append(names, c.Name+" "+dbType)
+	}
+	prefix := "CREATE TABLE "
+	if opt.IfNotExists {
+		prefix += "IF NOT EXISTS "
+	}
+	return prefix + name + " (" + strings.Join(names, ", ") + ")", nil
+
+}
+
+func (m Map) InsertStatement(tableName string) (string, []interface{}, error) {
+	cols, err := m.Columns()
+	if err != nil {
+		return "", nil, err
+	}
+	names := []string{}
+	idxs := []string{}
+	args := []interface{}{}
+	for i, c := range cols {
+		names = append(names, c.Name)
+		idxs = append(idxs, fmt.Sprintf("$%d", i+1))
+		args = append(args, c.Value)
+	}
+	return fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", tableName, strings.Join(names, ", "), strings.Join(idxs, ", ")), args, nil
+}
+
+func (m Map) Columns(names ...string) (ColumnValues, error) {
 	includer := func(name string) bool {
 		return true
 	}
@@ -18,31 +87,31 @@ func (m Map) Columns(names ...string) (Columns, error) {
 			return ok
 		}
 	}
-	out := Columns{}
+	out := ColumnValues{}
 	for k, v := range m {
 		if includer(k) {
-			out = append(out, &Column{Name: k, Value: v})
+			out = append(out, &ColumnValue{Name: k, Value: v})
 		}
 	}
 	sort.Sort(out)
 	return out, nil
 }
 
-type Columns []*Column
+type ColumnValues []*ColumnValue
 
-func (list Columns) Len() int {
+func (list ColumnValues) Len() int {
 	return len(list)
 }
 
-func (list Columns) Swap(a, b int) {
+func (list ColumnValues) Swap(a, b int) {
 	list[a], list[b] = list[b], list[a]
 }
 
-func (list Columns) Less(a, b int) bool {
+func (list ColumnValues) Less(a, b int) bool {
 	return list[a].Name < list[b].Name
 }
 
-func (list Columns) Names() []string {
+func (list ColumnValues) Names() []string {
 	out := []string{}
 	for _, c := range list {
 		out = append(out, c.Name)
@@ -50,7 +119,7 @@ func (list Columns) Names() []string {
 	return out
 }
 
-func (list Columns) Values() []interface{} {
+func (list ColumnValues) Values() []interface{} {
 	out := []interface{}{}
 	for _, c := range list {
 		out = append(out, c.Value)
@@ -58,7 +127,7 @@ func (list Columns) Values() []interface{} {
 	return out
 }
 
-type Column struct {
+type ColumnValue struct {
 	Name  string
 	Value interface{}
 }
