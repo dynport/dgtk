f83f0c09e1e89cb13141aef31d02b59b0645071f
Gereon Frey <gereon.frey@dynport.de>
2015-02-02 12:49:33 +0100

    retrieve docker host info

---
+++ dockerclient/docker_host.go
@@ -0,0 +1,34 @@
+package dockerclient
+
+type DockerHostInfo struct {
+	Containers         int
+	Images             int
+	Driver             string
+	DriverStatus       [][]string
+	ExecutionDriver    string
+	KernelVersion      string
+	NCPU               int
+	MemTotal           int
+	Name               string
+	ID                 string
+	Debug              int
+	NFd                int
+	NGoroutines        int
+	NEventsListener    int
+	InitPath           string
+	InitSha1           string
+	IndexServerAddress string
+	MemoryLimit        int
+	SwapLimit          int
+	IPv4Forwarding     int
+	Labels             []string
+	DockerRootDir      string
+	OperatingSystem    string
+}
+
+func (dh *DockerHost) HostInfo() (*DockerHostInfo, error) {
+	u := dh.url() + "/info"
+
+	dhi := &DockerHostInfo{}
+	return dhi, dh.getJSON(u, &dhi)
+}
