34b226c6117798011ea3c1cd23121ad3ed60b9f4
Tobias Schwab <tobias.schwab@dynport.de>
2015-05-16 16:43:48 +0200

    remove runtime dependency for mongodb

--- bsondecoder/decoder_test.go
+++ bsondecoder/decoder_test.go
@@ -3,21 +3,15 @@ package bsondecoder
 import (
 	"io"
 	"os"
-	"os/exec"
 	"testing"
 
-	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 )
 
 func TestScanner(t *testing.T) {
-	dir, e := setup(t)
-	if e != nil {
-		t.Fatal("error setting up")
-	}
-	f, e := os.Open(dir + "/albums.bson")
-	if e != nil {
-		t.Fatal("error opening fixture")
+	f, err := os.Open("fixtures/albums.bson")
+	if err != nil {
+		t.Fatalf("error opening fixture: %s", err)
 	}
 	defer f.Close()
 
@@ -25,12 +19,10 @@ func TestScanner(t *testing.T) {
 	albums := []*Album{}
 	for {
 		var a *Album
-		e = scanner.Decode(&a)
-		if e == io.EOF {
+		if err = scanner.Decode(&a); err == io.EOF {
 			break
-		}
-		if e != nil {
-			t.Error("error decoding album", e)
+		} else if err != nil {
+			t.Error("error decoding album", err)
 		}
 		albums = append(albums, a)
 	}
@@ -63,56 +55,3 @@ const (
 	colName = "albums"
 	dbName  = "bsonscanner-test"
 )
-
-func setup(t *testing.T) (string, error) {
-	e := func() error {
-		ses, e := mgo.Dial("127.0.0.1")
-		if e != nil {
-			return e
-		}
-		defer ses.Close()
-
-		db := ses.DB(dbName)
-		e = db.DropDatabase()
-		if e != nil {
-			return e
-		}
-		albums := []*Album{
-			{Artist: "Mos Def", Title: "Black on Both Sides"},
-			{Artist: "Gang Starr", Title: "Full Clip"},
-			{Artist: "A Tribe Called Quest", Title: "Low End Theory"},
-		}
-
-		for _, album := range albums {
-			album.Id = bson.NewObjectId()
-			e := db.C(colName).Insert(album)
-			if e != nil {
-				t.Fatal("error writing album", e)
-			}
-		}
-
-		cnt, e := db.C(colName).Find(nil).Count()
-		if e != nil {
-			t.Fatal("error counting albums", e)
-		}
-		if cnt != 3 {
-			t.Errorf("expected cnt to be 3, was %v", cnt)
-		}
-		if e = os.RemoveAll("tmp"); e != nil {
-			return e
-		}
-
-		if e = os.MkdirAll("tmp", 0755); e != nil {
-			return e
-		}
-
-		c := exec.Command("mongodump", "-d", dbName, "-o", ".")
-		c.Dir = "tmp"
-		c.Stderr = os.Stderr
-		return c.Run()
-	}()
-	if e != nil {
-		return "", e
-	}
-	return "tmp/" + dbName, nil
-}
