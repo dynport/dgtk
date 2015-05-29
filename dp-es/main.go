d79af88738fd1eccd1f5dadbb85e52596a92f28c
Tobias Schwab <tobias.schwab@dynport.de>
2015-05-29 15:11:03 +0200

    rename list to ls and remove to rm

--- dp-es/main.go
+++ dp-es/main.go
@@ -11,14 +11,14 @@ var logger = log.New(os.Stderr, "", 0)
 
 func main() {
 	router := cli.NewRouter()
-	router.Register("indexes/list", &esIndexes{}, "List es indexes")
-	router.Register("aliases/list", &esAliases{}, "List Aliases")
+	router.Register("indexes/ls", &esIndexes{}, "List es indexes")
+	router.Register("aliases/ls", &esAliases{}, "List Aliases")
 	router.Register("aliases/swap", &swapIndex{}, "Swap Alias")
 	router.Register("aliases/create", &aliasCreate{}, "Create alias")
-	router.Register("aliases/delete", &aliasDelete{}, "Delete alias")
-	router.Register("indexes/delete", &indexDelete{}, "Delete index")
-
+	router.Register("aliases/rm", &aliasDelete{}, "Delete alias")
+	router.Register("indexes/rm", &indexDelete{}, "Delete index")
 	router.Register("spy", &spy{}, "Spy on es requests")
+
 	switch e := router.RunWithArgs(); e {
 	case nil, cli.ErrorHelpRequested, cli.ErrorNoRoute:
 		// ignore
