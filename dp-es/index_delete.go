73df34ce94b7f62181cb9d4a57c652463cd29b25
Tobias Schwab <tobias.schwab@dynport.de>
2015-05-29 16:57:59 +0200

    indexes/rm: make interface variadic

--- dp-es/index_delete.go
+++ dp-es/index_delete.go
@@ -3,12 +3,17 @@ package main
 import "github.com/dynport/dgtk/es"
 
 type indexDelete struct {
-	Name string `cli:"arg required"`
-	Host string `cli:"opt -H default=127.0.0.1"`
+	Names []string `cli:"arg required"`
+	Host  string   `cli:"opt -H default=127.0.0.1"`
 }
 
 func (r *indexDelete) Run() error {
-	idx := &es.Index{Host: r.Host, Index: r.Name}
-	logger.Printf("deleting index %q", r.Name)
-	return idx.DeleteIndex()
+	for _, n := range r.Names {
+		idx := &es.Index{Host: r.Host, Index: n}
+		logger.Printf("deleting index %q", n)
+		if err := idx.DeleteIndex(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
