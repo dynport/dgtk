e26114afa9c632f8b3794fa51b8e588449fc7e7c
Tobias Schwab <tobias.schwab@dynport.de>
2015-05-30 16:27:00 +0200

    sequel: add uuid type

--- sequel/scan_uuid.go
+++ sequel/scan_uuid.go
@@ -1,9 +1,25 @@
 package sequel
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"strings"
 )
+
+type UUID string
+
+func (u *UUID) Scan(src interface{}) error {
+	if b, ok := src.([]byte); !ok {
+		return fmt.Errorf("src needs to be []byte")
+	} else {
+		*u = UUID(strings.Replace(string(b), "-", "", -1))
+		return nil
+	}
+}
+
+func (u UUID) Value() (driver.Value, error) {
+	return string(u), nil
+}
 
 func ScanUUID(s *string) *uuidScanner {
 	return &uuidScanner{ref: s}
