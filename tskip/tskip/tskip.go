034218af8259f11d38ab9314763e5a44b309fb54
Tobias Schwab <tobias.schwab@dynport.de>
2015-07-16 11:26:05 +0200

    actually use skip steps

--- tskip/tskip/tskip.go
+++ tskip/tskip/tskip.go
@@ -13,15 +13,15 @@ type ti interface {
 }
 
 func Errorf(t ti, skip int, m string, args ...interface{}) {
-	doError(t, 0, fmt.Sprintf(m, args...))
+	doError(t, 0+skip, fmt.Sprintf(m, args...))
 }
 
 func Error(t ti, skip int, m string) {
-	doError(t, 0, m)
+	doError(t, 0+skip, m)
 }
 
 func doError(t ti, skip int, m string) {
-	_, file, line, _ := runtime.Caller(3 + skip)
+	_, file, line, _ := runtime.Caller(1 + skip)
 	if tr := os.Getenv("TEST_ROOT"); tr != "" {
 		file = strings.TrimPrefix(file, tr+"/")
 	}
