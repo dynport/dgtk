e82efa3eff36604e519ba0dcd0fc9c5c91e2a2e7
Tobias Schwab <tobias.schwab@dynport.de>
2016-08-01 08:06:30 +0200

    gh repos/create: grant push access for team members (use --read-only to pull only)

--- github/gh/repos_create.go
+++ github/gh/repos_create.go
@@ -3,10 +3,11 @@ package main
 import "github.com/google/go-github/github"
 
 type reposCreate struct {
-	Name   string `cli:"arg required"`
-	Public bool   `cli:"opt --public"`
-	Orga   string `cli:"opt --orga"`
-	Teams  []int  `cli:"opt --teams"`
+	Name     string `cli:"arg required"`
+	Public   bool   `cli:"opt --public"`
+	Orga     string `cli:"opt --orga"`
+	Teams    []int  `cli:"opt --teams"`
+	ReadOnly bool   `cli:"opt --read-only desc='Add teams with read only'"`
 }
 
 func (r *reposCreate) Run() error {
@@ -19,8 +20,12 @@ func (r *reposCreate) Run() error {
 	if _, _, err := cl.Repositories.Create(r.Orga, repo); err != nil {
 		return err
 	}
+	permission := "push"
+	if r.ReadOnly {
+		permission = "pull"
+	}
 	for _, t := range r.Teams {
-		_, err = cl.Organizations.AddTeamRepo(t, r.Orga, r.Name, &github.OrganizationAddTeamRepoOptions{Permission: "pull"})
+		_, err = cl.Organizations.AddTeamRepo(t, r.Orga, r.Name, &github.OrganizationAddTeamRepoOptions{Permission: permission})
 		if err != nil {
 			return err
 		}
