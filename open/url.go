31c7c12b1d6eb1823d775041589f8e4fff12549b
Tobias Schwab <tobias.schwab@dynport.de>
2016-10-06 06:40:43 +0200

    open.URL: try to launch browser in the background

--- open/url.go
+++ open/url.go
@@ -2,13 +2,17 @@ package open
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os/exec"
 )
 
 func URL(s string) error {
 	for _, name := range []string{"xdg-open", "open"} {
 		if path, err := exec.LookPath(name); err == nil {
-			return exec.Command(path, s).Run()
+			s := exec.Command(path, s)
+			s.Stdout = ioutil.Discard
+			s.Stderr = ioutil.Discard
+			return s.Start()
 		}
 	}
 	return fmt.Errorf("no open command found")
