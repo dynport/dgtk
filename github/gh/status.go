a6db520826e9a8e6d181868879f73cf5a9c0f99b
Tobias Schwab <tobias.schwab@dynport.de>
2016-10-25 08:02:52 +0200

    status: correctly color pending commits, also add sha and time since build

--- github/gh/status.go
+++ github/gh/status.go
@@ -4,15 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os/exec"
+	"strings"
+	"time"
 
+	"github.com/dynport/dgtk/github"
 	"github.com/dynport/gocli"
 )
 
 type Status struct {
-	Ref string `cli:"opt --ref default=master"`
 }
 
 func (r *Status) Run() error {
+	branches := []string{"master"}
+	if cb, err := currentBranch(); err == nil && cb != "" {
+		branches = append([]string{cb}, "master")
+	}
 	repo, err := githubRepo()
 	if err != nil {
 		return err
@@ -21,31 +28,69 @@ func (r *Status) Run() error {
 	if err != nil {
 		return err
 	}
-	rsp, err := cl.Get("https://api.github.com/repos/" + repo + "/commits/" + r.Ref + "/status")
+
+	t := gocli.NewTable()
+	for _, b := range branches {
+		s, err := loadStatus(cl, repo, b)
+		if err != nil {
+			return err
+		}
+		var ago string
+		if len(s.Statuses) > 0 {
+			ago = strings.Split(time.Since(s.Statuses[0].CreatedAt).String(), ".")[0]
+		}
+		t.Add(b, colorizeStatus(s.State), truncate(s.SHA, 8, false), ago)
+	}
+	fmt.Println(t)
+	return nil
+}
+
+func colorizeStatus(in string) string {
+	color := gocli.Green
+	switch in {
+	case "success":
+		color = gocli.Green
+	case "pending":
+		color = gocli.Yellow
+	default:
+		color = gocli.Red
+	}
+	return color(in)
+}
+
+func loadStatus(cl *github.Client, repo, ref string) (res *statusResponse, err error) {
+	u := "https://api.github.com/repos/" + repo + "/commits/" + ref + "/status"
+	rsp, err := cl.Get(u)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer rsp.Body.Close()
 	if rsp.Status[0] != '2' {
 		b, _ := ioutil.ReadAll(rsp.Body)
-		return fmt.Errorf("got status %s but expected 2x. body=%s", rsp.Status, string(b))
+		return nil, fmt.Errorf("got status %s but expected 2x. body=%s", rsp.Status, string(b))
 	}
-	var res *statusResponse
 	err = json.NewDecoder(rsp.Body).Decode(&res)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	color := gocli.Green
-	switch res.State {
-	case "success":
-		color = gocli.Green
-	default:
-		color = gocli.Red
+	return res, nil
+}
+
+func currentBranch() (string, error) {
+	b, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").CombinedOutput()
+	if err != nil {
+		return "", err
 	}
-	fmt.Println(color(res.State))
-	return nil
+	return strings.TrimSpace(string(b)), nil
 }
 
 type statusResponse struct {
-	State string `json:"state"`
+	State    string `json:"state"`
+	Statuses []*struct {
+		URL       string    `json:"url,omitempty"`
+		TargetURL string    `json:"target_url,omitempty"`
+		CreatedAt time.Time `json:"created_at,omitempty"`
+		UpdatedAt time.Time `json:"updated_at,omitempty"`
+	} `json:"statuses"`
+	SHA string `json:"sha"`
 }
