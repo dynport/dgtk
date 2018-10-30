ae1db312505fb341259dc2e483715ed53a5fb9ec
Tobias Schwab <tobias.schwab@dynport.de>
2018-10-30 21:04:07 +0100

    go-reload: implement with fsnotify

--- go-reload/go-reload.go
+++ go-reload/go-reload.go
@@ -1,26 +1,26 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/exec"
+	"strings"
+	"sync"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
-var logger = func() *logrus.Logger {
-	l := logrus.New()
-	return l
-}()
-
 func main() {
-	if err := run(); err != nil {
-		logger.Fatalf("%+v", err)
+	l := logrus.New()
+	if err := run(l); err != nil {
+		l.Fatalf("%+v", err)
 	}
 }
 
-func run() error {
+func run(l logrus.FieldLogger) error {
 	if len(os.Args) < 2 {
 		return errors.New("at least 2 parameters required")
 	}
@@ -28,52 +28,51 @@ func run() error {
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	for {
-		err := func() error {
-			stat, err := os.Stat(path)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			modified := stat.ModTime()
-			options := []string{}
-			if len(os.Args) > 2 {
-				options = os.Args[2:]
-			}
-			var c *exec.Cmd
-			for i := 0; i < 10; i++ {
-				err := func() error {
-					c = exec.Command(path, options...)
-					c.Stdout = os.Stdout
-					c.Stdin = os.Stdin
-					c.Stderr = os.Stderr
-					return errors.WithStack(c.Start())
-				}()
-				if err != nil {
-					time.Sleep(1 * time.Second)
-				} else {
-					break
-				}
-			}
-			logger.Printf("running with pid %d", c.Process.Pid)
-			for {
-				if stat, err := os.Stat(path); err != nil {
-					logger.Printf("ERROR: %s", err)
-				} else if stat.ModTime() != modified {
-					logger.Printf("mod time changed => sleeping")
-					break
-				}
-				time.Sleep(1 * time.Second)
-			}
-			logger.Printf("killing pid %d", c.Process.Pid)
-			if err := c.Process.Kill(); err != nil {
-				return errors.WithStack(err)
-			}
-			c.Wait()
-			return nil
-		}()
-		if err != nil {
-			logger.Warn(err)
-			time.Sleep(1 * time.Second)
-		}
+
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return errors.WithStack(err)
 	}
+	err = w.Add(path)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	options := []string{}
+	if len(os.Args) > 2 {
+		options = os.Args[2:]
+	}
+
+	var (
+		ctx context.Context
+		cf  func()
+		mux sync.Mutex
+	)
+
+	go func() {
+		for {
+			l.Printf("starting up")
+			mux.Lock()
+			ctx, cf = context.WithCancel(context.Background())
+			mux.Unlock()
+			c := exec.CommandContext(ctx, path, options...)
+			c.Stdout = os.Stdout
+			c.Stdin = os.Stdin
+			c.Stderr = os.Stderr
+			err = c.Run()
+			if err != nil {
+				if !strings.Contains(err.Error(), "signal: killed") {
+					l.Warnf("%T %v", err, err)
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	for _ = range w.Events {
+		mux.Lock()
+		cf()
+		mux.Unlock()
+	}
+	return nil
 }
